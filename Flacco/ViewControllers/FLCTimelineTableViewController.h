d48cb116e4fd9abd9bd645ac48316095a884e359
Gianni Settino <gianni@wherecloud.com>
2014-05-22 11:32:51 +0200

    Basic setup for Timeline viewController

---
+++ Flacco/ViewControllers/FLCTimelineTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLCTimelineTableViewController.h
+//  Flacco
+//
+//  Created by Gianni Settino on 2014-05-22.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLCTimelineTableViewController : UITableViewController
+
+@end
